0b9f6d3013aa98bab0495b337185f1e7d7b81634
Gretchen Miller <gapmiller@gmail.com>
2016-06-11 11:53:28 -0700

    first commit

---
+++ windows/dir_list.py
@@ -0,0 +1,8 @@
+import os
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+
+list = get_immediate_subdirectories("L:\Engineering\IBEXWIN")
+print (list)
